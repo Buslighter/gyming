dbf39f10f23e1e706250ce396b8b74763db16477
Buslighter <gelbcka@gmail.com>
2023-02-01 15:29:40 +0300

    first commit

---
+++ gyming/gymingApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymingApp.swift
+//  gyming
+//
+//  Created by gleba on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct gymingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
